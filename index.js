608910359cf46261c3a6aac71fc580752ecc7733
Evan Miller <eamiller@dhcp-asv-100.helsinki.fi>
2018-02-09 13:49:06 +0200

    3.7-3.8

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const morgan = require('morgan')
+
+morgan.token('reqbody', function (req, res) { return JSON.stringify(req.body) })
+app.use(
+    morgan(function (tokens, req, res) {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens['reqbody'](req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'), '-',
+            tokens['response-time'](req, res), 'ms'
+        ].join(' ')
+    })
+)
 
 app.use(bodyParser.json())
 
